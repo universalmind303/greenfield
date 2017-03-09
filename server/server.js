32deabdcdaaf737cd0d6b2f149201c2ecba24a7a
Elijah Schow <elijah.schow@gmail.com>
2017-03-09 15:46:43 -0600

    Let the environment specify which directory to serve

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
+const directory = process.env.PUBLIC || 'public';
 
-app.use('/', express.static('./public'));
-app.listen(port, () => { console.log(`Listening on port ${port}`) });
+app.use('/', express.static(directory));
+app.listen(port, () => { console.log(`Listening on port ${port}. Serving '${directory}'.`) });
--- server/tests/ServerSpec.js
+++ server/tests/ServerSpec.js
@@ -4,6 +4,7 @@ const server = require('./server');
 describe('Static File Server', () => {
   it( 'should listen on port 8080 by default', () => { /* no-op */ });
   it( 'should listen on `process.env.PORT` if specified', () => { /* no-op */ });
-  it( 'should serve files from the public directory', () => { /* no-op */ });
+  it( 'should serve files from the `public` directory by default', () => { /* no-op */ });
+  it( 'should serve files from the `process.env.PUBLIC` if specified', () => { /* no-op */ });
   it( 'should respond with 404 for non-existant files', () => { /* no-op */ });
 });
