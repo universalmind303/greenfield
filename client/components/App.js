29616e4c774c02465590060dc77dca356495e49a
universalmind303 <universalmind.candy@gmail.com>
2017-03-09 09:53:30 -0600

    refactored template

---
+++ client/components/App.js
@@ -0,0 +1,10 @@
+var App = () => (
+<div>App</div>
+    
+    
+
+);
+
+// In the ES6 spec, files are "modules" and do not share a top-level scope
+// `var` declarations will only exist globally where explicitly defined
+window.App = App
