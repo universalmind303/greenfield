0822f6c1f7955a9aa81c0141ba33061f113c148c
universalmind303 <universalmind.candy@gmail.com>
2017-03-09 18:17:50 -0600

    enviroment working properly with multiple files

--- client/components/App.js
+++ client/components/App.js
@@ -1,10 +1,17 @@
-var App = () => (
-<div>Grocery Store</div>
-    
-    
+class App extends React.Component { 
+constructor(props){
+  super(props)
 
-);
 
-// In the ES6 spec, files are "modules" and do not share a top-level scope
-// `var` declarations will only exist globally where explicitly defined
-window.App = App
+}
+
+  render() {
+    return (
+
+      <div>
+      <Test />
+      </div>
+    )
+  }
+};
+window.App = App;
