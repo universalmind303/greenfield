fe4475c9125e46cbc39ceb0f4702d52ff25da81e
universalmind303 <universalmind.candy@gmail.com>
2017-03-13 20:21:20 -0500

    minor adjustments, also refactored listitem and header to be stateless components

--- client/source/components/ListItem.jsx
+++ client/source/components/ListItem.jsx
@@ -3,37 +3,23 @@ import App from './App.jsx'
 import InlineEdit from './InlineEdit.jsx'
 
 
-export default class ListItem extends React.Component {
-  constructor(props){
-    super(props)
-  }
-
-  // updateItemInfo(val, ref) {
-  //   console.log(ref);
-  //   return val;
-  // }
-
-  render() {
-    return (
-      <div className="listItems">
-        {this.props.list.map(function(item) {
-          // return <h2 key={item.name}>{item.name + ": $" + item.price}</h2>
-          return (
-            <div className="listItem">
-              <InlineEdit
-                key={item.id}
-                text={item.name}
-                updateName={this.props.updateName}
-              />
+export default function ListItem (props) {
+  return (
+    <div className="listItems">
+      {props.list.map(function(item, index) {
+        return (
+          <div className="listItem">
             <InlineEdit
-                key={item.id}
-                text={'$ ' + item.price}
-                updatePrice={this.props.updatePrice}
-              />
-            </div>
-          )
-        }, this)}
-      </div>
-    )
-  }
+              text={item.name}
+              updateName={props.updateName}
+            />
+          <InlineEdit
+              text={'$ ' + item.price}
+              updatePrice={props.updatePrice}
+            />
+          </div>
+        )
+      })}
+    </div>
+  )
 }
--- client/tests/AppSpec.test.jsx
+++ client/tests/AppSpec.test.jsx
@@ -1,150 +1,150 @@
-// import React, {Component} from 'react';
-// import { expect, should, assert } from 'chai';
-// import { mount, shallow, render } from 'enzyme';
-// import TestUtils from 'react-addons-test-utils';
-// import ReactDOM from 'react-dom';
-// import sinon from 'sinon'
+import React, {Component} from 'react';
+import { expect, should, assert } from 'chai';
+import { mount, shallow, render } from 'enzyme';
+import TestUtils from 'react-addons-test-utils';
+import ReactDOM from 'react-dom';
+import sinon from 'sinon'
 
-// import App  from '../source/components/App.jsx';
-// import Header from '../source/components/Header.jsx';
-// import ListItem from '../source/components/ListItem.jsx';
-// import InlineEdit from '../source/components/InlineEdit.jsx';
-
-
+import App  from '../source/components/App.jsx';
+import Header from '../source/components/Header.jsx';
+import ListItem from '../source/components/ListItem.jsx';
+import InlineEdit from '../source/components/InlineEdit.jsx';
 
 
-// describe('File structure', ()=>{
-//   it('Header should be contained in its own file', ()=> {
-//     expect(Header).to.be.a.function
-//   });
-//  it('ListItem should be contained in its own file', ()=> {
-//     expect(ListItem).to.be.a.function
-//   });
-//   it('InlineEdit should be contained in its own file', ()=> {
-//     expect(InlineEdit).to.be.a.function
-//   });
-// })
 
-// describe('App', function() {
-//   var app;
 
-//   it('should be a stateful class component', function() {
-//     expect(React.Component.isPrototypeOf(App)).to.be.true;
-//   });
+describe('File structure', ()=>{
+  it('Header should be contained in its own file', ()=> {
+    expect(Header).to.be.a.function
+  });
+ it('ListItem should be contained in its own file', ()=> {
+    expect(ListItem).to.be.a.function
+  });
+  it('InlineEdit should be contained in its own file', ()=> {
+    expect(InlineEdit).to.be.a.function
+  });
+})
+
+describe('App', function() {
+  var app;
+
+  it('should be a stateful class component', function() {
+    expect(React.Component.isPrototypeOf(App)).to.be.true;
+  });
   
-//   it('App should render a div',()=> {
-//     app =shallow(<App />);  
-//     expect(app.node.type).to.equal('div');
-//     app.unmount()
-//   })
+  it('App should render a div',()=> {
+    app =shallow(<App />);  
+    expect(app.node.type).to.equal('div');
+    app.unmount()
+  })
 
-//   it('Components should have a div class',function(){
-//     app = render(<App />)  
-//     expect(app.find(".app").attr()).to.deep.equal({class: 'app'})
-//     expect(app.find(".header").attr()).to.deep.equal({class: 'header'})
-//     expect(app.find(".listItems").attr()).to.deep.equal({class: 'listItems'})
-//   });
+  it('Components should have a div class',function(){
+    app = render(<App />)  
+    expect(app.find(".app").attr()).to.deep.equal({class: 'app'})
+    expect(app.find(".header").attr()).to.deep.equal({class: 'header'})
+    expect(app.find(".listItems").attr()).to.deep.equal({class: 'listItems'})
+  });
 
-//   it('should increase total, and reset budget', function(){
-//     app = mount(<App />)
-//     expect(app.find('.app').text()).to.equal(' Budget $ 0 Total $ 0')
-//     app.node.setState({budget: 10, total: 10})
-//     app.node.updateBudget(10)
-//     app.node.updateTotal(10)
-//     expect(app.find('.app').text()).to.equal(' Budget $ 10 Total $ 20')
-//     app.unmount()
-//   });
-//   it('should only accept numbers as price inputs', function(){
+  it('should increase total, and reset budget', function(){
+    app = mount(<App />)
+    expect(app.find('.app').text()).to.equal(' Budget $ 0 Total $ 0')
+    app.node.setState({budget: 10, total: 10})
+    app.node.updateBudget(10)
+    app.node.updateTotal(10)
+    expect(app.find('.app').text()).to.equal(' Budget $ 10 Total $ 20')
+    app.unmount()
+  });
+  it('should only accept numbers as price inputs', function(){
 
-//       //it should handle truthy/falsy inputs, i fixed it to ignore all except truthy/falsy
-//     app = mount(<App />)
-//     var num = 11
-//     app.node.updateBudget(num)
-//     app.node.updateBudget('an invalid input')
-//     app.node.updateBudget({})
-//     app.node.updateBudget({fake: 'data'})
-//     app.node.updateBudget(['more bad data', num])
-//     app.node.updateBudget(true)
-//     app.node.updateBudget(false)
-//     app.node.updateBudget(undefined)
-//     expect(app.node.state['budget']).to.equal(num)
+      //it should handle truthy/falsy inputs, i fixed it to ignore all except truthy/falsy
+    app = mount(<App />)
+    var num = 11
+    app.node.updateBudget(num)
+    app.node.updateBudget('an invalid input')
+    app.node.updateBudget({})
+    app.node.updateBudget({fake: 'data'})
+    app.node.updateBudget(['more bad data', num])
+    app.node.updateBudget(true)
+    app.node.updateBudget(false)
+    app.node.updateBudget(undefined)
+    expect(app.node.state['budget']).to.equal(num)
 
-//     app.node.updateTotal(num)
-//     app.node.updateTotal('an invalid input')
-//     app.node.updateTotal({})
-//     app.node.updateTotal({fake: 'data'})
-//     app.node.updateTotal(['more bad data', num])
-//     app.node.updateTotal(true)
-//     app.node.updateTotal(false)
-//     app.node.updateTotal(undefined)
-//     expect(app.node.state['total']).to.equal(num)
-//     app.unmount()
-//   });
-//   it('should handle invalid inputs', function(){
-//     app = mount(<App/>)
-//     app.node.addListItem('strawberry',10)
-//     app.node.addListItem({}, {})
-//     app.node.addListItem(1, 'strawberry')
-//     app.node.addListItem(null, null)
-//     expect(app.node.state['list'].length).to.equal(1)
-//     app.unmount()
+    app.node.updateTotal(num)
+    app.node.updateTotal('an invalid input')
+    app.node.updateTotal({})
+    app.node.updateTotal({fake: 'data'})
+    app.node.updateTotal(['more bad data', num])
+    app.node.updateTotal(true)
+    app.node.updateTotal(false)
+    app.node.updateTotal(undefined)
+    expect(app.node.state['total']).to.equal(num)
+    app.unmount()
+  });
+  it('should handle invalid inputs', function(){
+    app = mount(<App/>)
+    app.node.addListItem('strawberry',10)
+    app.node.addListItem({}, {})
+    app.node.addListItem(1, 'strawberry')
+    app.node.addListItem(null, null)
+    expect(app.node.state['list'].length).to.equal(1)
+    app.unmount()
 
-//   })
-//   it('should be able to add list items', function(){
-//     app = mount(<App/>)
-//     app.node.addListItem('strawberry',10)
-//     expect(app.node.state['list'].length).to.equal(1)
-//     app.unmount()
+  })
+  it('should be able to add list items', function(){
+    app = mount(<App/>)
+    app.node.addListItem('strawberry',10)
+    expect(app.node.state['list'].length).to.equal(1)
+    app.unmount()
 
-//   })
+  })
 
-//   xit('should submit form on click', function() {
-//   });
-// });
-
-
-// describe('List', ()=>{
-//   var app;
-//   var list;
-//   it('should render list items', ()=>  {
-//     list = shallow(<ListItem list={[{name: 'apple', price: 2},{name: 'orange', price: 3}]}/>)
-//     expect(list.node.type).to.equal('div')
-//     expect(list.find('.listItems').children().length).to.equal(2)
-//     expect(list.find('.listItems').html()).to.exist
-//     list.unmount()
-//   });
+  xit('should submit form on click', function() {
+  });
+});
 
 
+describe('List', ()=>{
+  var app;
+  var list;
+  it('should render list items', ()=>  {
+    list = shallow(<ListItem list={[{name: 'apple', price: 2},{name: 'orange', price: 3}]}/>)
+    expect(list.node.type).to.equal('div')
+    expect(list.find('.listItems').children().length).to.equal(2)
+    expect(list.find('.listItems').html()).to.exist
+    list.unmount()
+  });
 
-//   xit("should handle 2 of the same inputs", ()=> {
-//     // not 100% on how you should handle this one, but it definitly needs to handle 2 of the same items.
-//   list = shallow(<ListItem list={[{name: 'apple', price: 2}, {name: 'apple', price: 2}]}/>)
 
 
-//   // i suggest increasing the prices
-//   expect(list.find('.listItems').find('listItem').text()).to.equal('apple: $4')
-//   expect(list.find('.listItems').children().length).to.equal(1)
-//   list.unmount()
-//   });
+  xit("should handle 2 of the same inputs", ()=> {
+    // not 100% on how you should handle this one, but it definitly needs to handle 2 of the same items.
+  list = shallow(<ListItem list={[{name: 'apple', price: 2}, {name: 'apple', price: 2}]}/>)
 
 
-// })
+  // i suggest increasing the prices
+  expect(list.find('.listItems').find('listItem').text()).to.equal('apple: $4')
+  expect(list.find('.listItems').children().length).to.equal(1)
+  list.unmount()
+  });
 
-// describe("Budget", ()=> {
-//   var header;
-//   var app;
 
-//   beforeEach(() =>{
-//     header = mount(<Header />)
-//     app = mount(<App />)
-//   })
-//   afterEach(() => {
-//     header.unmount()
-//     app.unmount()
-//   })
+})
 
-//   it('should only accept numbers as input', () =>{
-//   })
-// })
+describe("Budget", ()=> {
+  var header;
+  var app;
+
+  beforeEach(() =>{
+    header = mount(<Header />)
+    app = mount(<App />)
+  })
+  afterEach(() => {
+    header.unmount()
+    app.unmount()
+  })
+
+  it('should only accept numbers as input', () =>{
+  })
+})
 
 
