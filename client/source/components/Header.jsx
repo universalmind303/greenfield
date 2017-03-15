4b6b2b53dbe24c0d1f8527268925b547f07b13f8
universalmind303 <universalmind.candy@gmail.com>
2017-03-15 14:47:11 -0500

    budget header changes at 90%

--- client/source/components/Header.jsx
+++ client/source/components/Header.jsx
@@ -3,8 +3,17 @@ import App from './App.jsx'
 import InlineEdit from './InlineEdit.jsx'
 
 export default ({budget, updateBudget, total}) => {
+  let header = 'onBudget'
+  if(budget) {
+    if(total >= (budget * .9) && total < budget) {
+      header = "warningBudget";
+    } else if( total >= budget) {
+      header = 'overBudget'
+    } 
+  }
+
   return (
-    <div className="header">
+    <div className={header}>
       <div className="statBlock half">
         <div className="label">Budget</div>
         <div className="value">
