bdb65cb7a60ca4b42f586590adbbe0fe1ea08b5a
Elijah Schow <elijah.schow@gmail.com>
2017-03-16 14:37:42 -0500

    (bug) fix header spacing

--- client/source/components/Header.jsx
+++ client/source/components/Header.jsx
@@ -3,13 +3,13 @@ import App from './App.jsx'
 import InlineEdit from './InlineEdit.jsx'
 
 export default ({budget, updateBudget, total}) => {
-  let header = 'onBudget'
+  let header = 'onBudget header'
   if(budget) {
     if(total >= (budget * .9) && total < budget) {
-      header = "warningBudget";
+      header = "warningBudget header";
     } else if( total >= budget) {
-      header = 'overBudget'
-    } 
+      header = 'overBudget header'
+    }
   }
 
   return (
