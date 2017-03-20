cf803d8ac1431f36c818407920ab94346feb436e
universalmind303 <universalmind.candy@gmail.com>
2017-03-20 10:55:42 -0500

    (enhancement) cleaned up testing suite.

--- server/tests/ServerSpec.js
+++ server/tests/ServerSpec.js
@@ -6,13 +6,13 @@ const express = require('express');
 const fs = require('fs');
 const Promise = require('bluebird')
 Promise.promisifyAll(fs)
-xdescribe('Static File Server', () => {
+describe('Static File Server', () => {
 
 
 
 
   it( 'should listen on port 8080 by default', (done) => {
-   request(baseUrl, function(err, resp, body) {
+   request('http://127.0.0.1:8080', function(err, resp, body) {
       assert(resp.statusCode === 200);
       done();
     });
@@ -36,13 +36,5 @@ xdescribe('Static File Server', () => {
       done();
     });
   });
-  xit( 'should serve files from the `process.env.PUBLIC` if specified', (done) => {
-   // request(`${baseUrl}/server/tests/fakePublic/index.html`, function(err, resp, body) {
-   //    fs.readFileAsync('/fakePublic/index.html', 'utf8').then(file=>{
-   //    expect(body).to.deep.equal(file) 
-   //    });
-   //    done();
-   //  });
-  });
-  xit( 'should respond with 404 for non-existant files', (done) => { /* no-op */ });
+
 });
