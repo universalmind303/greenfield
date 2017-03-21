ebeab3989648a0efb9debac004ac3a2c643d677f
universalmind303 <universalmind.candy@gmail.com>
2017-03-20 19:57:58 -0500

    es6 destructure listitem

--- client/source/components/ListItem.jsx
+++ client/source/components/ListItem.jsx
@@ -2,18 +2,18 @@ import React from 'react';
 import App from './App.jsx';
 import InlineEdit from './InlineEdit.jsx';
 
-export default function ListItem ({item, removeItem, updateItem, autoFocus,focused}){
+export default function ListItem ({{key,price,name}, removeItem, updateItem, autoFocus,focused}){
   return (
     <tr className="listItem">
       <td className="delete">
-        <button onClick={() => removeItem(item.key)}>
+        <button onClick={() => removeItem(key)}>
           <i className="fa fa-trash" aria-hidden="true"></i>
         </button>
       </td>
       <td className="name">
         <InlineEdit
-          value={item.name}
-          action={value => updateItem(item.key, "name", value)}
+          value={name}
+          action={value => updateItem(key, "name", value)}
           type="text"
           placeholder="Item"
           autoFocus={autoFocus}
@@ -21,8 +21,8 @@ export default function ListItem ({item, removeItem, updateItem, autoFocus,focus
       </td>
       <td className="price">
         <InlineEdit
-          value={item.price}
-          action={value => updateItem(item.key, "price", value)}
+          value={price}
+          action={value => updateItem(key, "price", value)}
           type="number"
           prefix="$&nbsp;"
           placeholder="$ 0"
